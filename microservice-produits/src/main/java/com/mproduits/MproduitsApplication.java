51b12a0bb46694e9e264d772e10ce78db1ccc8d4
fouomenedaniel <fouomenedaniel@gmail.com>
2022-04-25 16:34:54 +0100

    Externalisation la configuration de vos microservices avec Spring Cloud Config

---
+++ microservice-produits/src/main/java/com/mproduits/MproduitsApplication.java
@@ -0,0 +1,14 @@
+package com.mproduits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+public class MproduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MproduitsApplication.class, args);
+	}
+}
